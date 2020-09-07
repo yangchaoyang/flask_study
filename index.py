841e7abbc041fee77f9ac16896756c973f520bff
yangchaoyang <cy_yang9090@163.com>
2020-09-07 22:46:34 +0800

    '简单的werkzeug原理'

---
+++ index.py
@@ -0,0 +1,14 @@
+from werkzeug.wrappers import Request, Response
+class Shortly(object):
+    def __call__(self, environ, start_response):
+        # start_response('200 ok', [('Content-Type', 'text/plain')])
+        # return [b'hello world~~']
+        request = Request(environ)
+        text = 'hello, world!  %s'%request.args.get('a', 'yang')
+        response  = Response(text, mimetype='text/plain')
+        return response(environ, start_response)
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple 
+    app = Shortly()
+    run_simple('0.0.0.0', 5000, app)
